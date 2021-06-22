e3a652fab38a2a4b24f199120790051635bfd89a
zuran21 <81603223+zuran21@users.noreply.github.com>
2021-06-21 20:28:48 -0500

    subido
    ignorar el java 2

--- JS/java2.js
+++ JS/java2.js
@@ -1,35 +1,4 @@
-function ingresar(){
-    var correo=document.getElementById('correo').value;
-    var enviar=document.getElementById('enviar').value;
-    var contraseña=document.getElementById('password').value;
-    
-
-    if(correo=="#"){
-        alert('success');
-        window.open('GraciasporSuscribirte.html')
-    }else{
-        alert('Por favor ingrese su correo');
-    }
-    if(enviar==""){
-        alert('Porfavor rellene los campos anteriores');
-    }
-    if(contraseña==""){
-        alert('Porfavor ingrese su contraseña');
-    }
-    if(registro==""){
-        alert ('faltan llenar datos');
-    }
-    
-}
-
-  function sololetras(event){
-    if (event.keyCode >48 && event.keyCode  <62) event.returnValue = false;
- } 
- function solonumeros(event){
-   if (event.keyCode >65 && event.keyCode <126) event.returnValue = false;
-
-  }
-  function registrar(){
+function registrar(){
     var registro=document.getElementById('registrar1').value;
     var nnombre=document.getElementById('nombre').value;
     var apellido=document.getElementById('apellido').value;
